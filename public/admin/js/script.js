58b5bd9a87f97a8509b643dedc102ec89aa26829
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-08-20 19:30:40 +0530

    small updates

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -109,6 +109,26 @@ $(function() {
         plugins: 'advlist link image lists table',
         automatic_uploads: true,
         images_upload_url: "/tinymce/upload/",
+        file_picker_callback: function(cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+            input.onchange = function() {
+                var file = this.files[0];
+
+                var reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = function () {
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+                    cb(blobInfo.blobUri(), { title: file.name });
+                };
+            };
+            input.click();
+        },
         toolbar: 'undo redo | formatselect | ' +
         'bold italic backcolor | alignleft aligncenter ' +
         'alignright alignjustify | bullist numlist outdent indent | ' +
