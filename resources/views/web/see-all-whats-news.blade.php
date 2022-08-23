ecab212634479c9327c5aa534f0e32431fce4e74
CYBER NETICS <cybernetics.me@outlook.com>
2022-08-23 23:44:39 +0530

    small updates

--- resources/views/web/see-all-whats-news.blade.php
+++ resources/views/web/see-all-whats-news.blade.php
@@ -4,11 +4,11 @@
 <div role="main" class="main">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 pt-3 bg-eee">
+            <div class="col-lg-12 pt-3 bg-eee mb-3">
                 <h2>See All What's New</h2>
             </div>
             @forelse($news as $key => $ne)
-            <div class="col-md-12 mt-3">
+            <div class="col-md-12">
                 <ul class="list list-icons list-primary list-borders">
                     @if($ne->doc_type == 'url')
                         <li><i class="fa fa-check"></i><a href="{{ $ne->url }}" target="_blank">{{ $ne->title }}</a></li>
