89892e267b6b302c688d9c9e882b311d34a99e58
CYBER NETICS <cybernetics.me@outlook.com>
2022-08-24 09:28:59 +0530

    small updates

--- resources/views/web/photo-gallery.blade.php
+++ resources/views/web/photo-gallery.blade.php
@@ -7,7 +7,7 @@
             <div class="col-lg-12 pt-3 bg-eee">
                 <h2>Photo Gallery</h2>
             </div>
-            @forelse($albums as $key => $album)
+            <!--@forelse($albums as $key => $album)
             @php $image = DB::table('album_images')->where('album_id', $album->id)->get()->first(); @endphp
             <div class="col-lg-4 mt-3">
                 <div class="card">
@@ -18,15 +18,15 @@
                 </div>
             </div>
             @empty
-            @endforelse
-            <!--@forelse($albums as $key => $album)
+            @endforelse-->
+            @forelse($albums as $key => $album)
                 @php $images = DB::table('album_images')->where('album_id', $album->id)->get(); @endphp
                 <div class="col-lg-12">
                     <h4>{{ $album->album_title }}</h4>
                     <div class="lightbox" data-plugin-options="{'delegate': 'a', 'type': 'image', 'gallery': {'enabled': true}, 'mainClass': 'mfp-with-zoom', 'zoom': {'enabled': true, 'duration': 300}}">
-						<div class="owl-carousel owl-theme stage-margin" data-plugin-options="{'items': 6, 'margin': 10, 'loop': false, 'nav': true, 'dots': false, 'stagePadding': 40}">
+						<div class="owl-carousel owl-theme stage-margin" data-plugin-options="{'items': 1, 'margin': 10, 'loop': false, 'nav': true, 'dots': false, 'stagePadding': 40}">
                             @foreach($images as $key => $img)
-                            <div>
+                            <div class="card">
                                 <a class="img-thumbnail img-thumbnail-no-borders img-thumbnail-hover-icon" href="{{ public_path().'/storage/albums/'.$img->album_id.'/'.$img->img_name }}">
                                     <img class="img-fluid" src="{{ public_path().'/storage/albums/'.$img->album_id.'/'.$img->img_name }}" alt="Project Image">
                                 </a>
@@ -36,7 +36,7 @@
                     </div>
                 </div>
             @empty
-            @endforelse-->
+            @endforelse
         </div>
     </div>
 </div>
