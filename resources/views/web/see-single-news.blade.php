2de8b4954f86c097b9eb5ada8f159cfadf6f5917
CYBER NETICS <cybernetics.me@outlook.com>
2022-08-26 08:23:12 +0530

    small updates

--- resources/views/web/see-single-news.blade.php
+++ resources/views/web/see-single-news.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 pt-3 bg-eee">
-                <h4 class="px-3 text-white">{{ ucwords(trans($news->page_title)) }}</h4>
+                <h4 class="px-3 text-white">{{ $news->page_title }}</h4>
             </div>
             <div class="col-md-12 mt-3">
                 <div class="post-image">
