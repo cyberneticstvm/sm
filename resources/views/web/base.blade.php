95f43de6b61a5c9093897dac603d997b5ea4b1c1
CYBER NETICS <cybernetics.me@outlook.com>
2022-09-03 09:23:57 +0530

    small updates

--- resources/views/web/base.blade.php
+++ resources/views/web/base.blade.php
@@ -164,7 +164,10 @@
 															@elseif($submenu && $value->menu_category == 'single')
 																<ul class="dropdown-menu">
 																	@foreach($submenu as $skey => $sval)
-																		<li><a class="dropdown-item" href="">{{ $sval->menu_item_name }}</a></li>
+																		@php 
+																		$nextmenu = DB::table('menus')->where('parent', $sval->id)->get();
+																		$slug = DB::table('pages')->where('id', $sval->page_id)->value('slug'); @endphp
+																		<li><a class="dropdown-item" href="{{ ($sval->page_id == 0) ? '/'.$nextmenu->page_url : '/web/'.$slug.'/' }}">{{ $sval->menu_item_name }}</a></li>
 																	@endforeach
 																</ul>
 															@endif
