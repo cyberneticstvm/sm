3b46ca6195ff2ceac679b8660761c2a546455324
CYBER NETICS <cybernetics.me@outlook.com>
2022-09-10 17:31:46 +0530

    small updates

--- resources/views/web/photo-gallery-lightbox.blade.php
+++ resources/views/web/photo-gallery-lightbox.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <div class="lightbox" data-plugin-options="{'delegate': 'a', 'type': 'image', 'gallery': {'enabled': false}, 'mainClass': 'mfp-with-zoom', 'zoom': {'enabled': true, 'duration': 300}}">
+                <div class="lightbox" data-plugin-options="{'delegate': 'a', 'type': 'image', 'gallery': {'enabled': true}, 'mainClass': 'mfp-with-zoom', 'zoom': {'enabled': true, 'duration': 300}}">
                     <div class="owl-carousel owl-theme stage-margin" data-plugin-options="{'items': 3, 'margin': 10, 'loop': false, 'nav': false, 'dots': false, 'stagePadding': 40}">                            
                         @foreach($images as $key => $img)
                         <div class="card-body col-lg-12">
